b0637e9c315983004acf43db04020d4c83ad3809
Guilherme Vale <guilherme.vale98@gmail.com>
2023-11-18 01:15:50 -0300

    First Commit

---
+++ src/main/java/com/gui/schoolsysbackend/SchoolSysBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gui.schoolsysbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSysBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolSysBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gui/schoolsysbackend/SchoolSysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gui.schoolsysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSysBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
